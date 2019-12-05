9db258d67ec60f0ff64ef6c003dcba2b8b9c9f8c
Giovanny Cerna <gcerna@csumb.edu>
2019-12-05 12:20:29 -0800

    Working example

--- app/src/main/java/com/example/pokemonchallenge/MainActivity.kt
+++ app/src/main/java/com/example/pokemonchallenge/MainActivity.kt
@@ -1,70 +1,67 @@
 package com.example.pokemonchallenge
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
 import android.widget.TextView
-import com.google.gson.annotations.SerializedName
-import kotlinx.android.synthetic.main.activity_main.*
-import retrofit2.Retrofit
-import retrofit2.Call
-import retrofit2.Response
-import retrofit2.Callback
-import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.http.GET
-import retrofit2.http.Path
+import androidx.appcompat.app.AppCompatActivity
+import com.squareup.picasso.Picasso
+import okhttp3.*
+import org.json.JSONArray
+import org.json.JSONObject
 import java.io.IOException
-import java.net.URI
-
-
 class MainActivity : AppCompatActivity() {
-   // var id = null
-//https://api.pokemontcg.io/v1/cards
+    lateinit var test: TextView
+    lateinit var test2: ImageView
+    var arrayListDetails:ArrayList<Model> = ArrayList()
+    //OkHttpClient creates connection pool between client and server
+    val client = OkHttpClient()
     override fun onCreate(savedInstanceState: Bundle?) {
-       super.onCreate(savedInstanceState)
-       setContentView(R.layout.activity_main)
-       var id = findViewById<TextView>(R.id.card)
-       //var pic = findViewById<ImageView>(R.id.imageView1)
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        test = this.findViewById(R.id.card)
+        test2 = this.findViewById(R.id.card1)
+        run("https://api.pokemontcg.io/v1/cards")
+    }
+    fun run(url: String) {
+        val request = Request.Builder()
+            .url(url)
+            .build()
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+            }
+            override fun onResponse(call: Call, response: Response) {
+                var strResponse = response.body()!!.string()
+                //creating json object
+                val jsonContact:JSONObject = JSONObject(strResponse)
+                //creating json array
+                var jsonarrayInfo:JSONArray= jsonContact.getJSONArray("cards")
+                var i:Int = 0
+                var size:Int = jsonarrayInfo.length()
+                for (i in 0 until size) {
+                    var jsonObjectDetail:JSONObject = jsonarrayInfo.getJSONObject(i)
+                    var model:Model= Model()
+                    model.image = jsonObjectDetail.getString("imageUrl").toString()
+                    model.onclick = jsonObjectDetail.getString("imageUrlHiRes").toString()
+                    arrayListDetails.add(model)
+                }
+                runOnUiThread {
+                    // Stuff that updates the UI
+                    test.text = arrayListDetails.size.toString()
+                    val picasso = Picasso.get()
+                    picasso.load(arrayListDetails.get(0).image)
+                        .into(test2)
 
-       val retrofit = Retrofit.Builder()
-           .baseUrl("https://api.pokemontcg.io/v1/")
-           .addConverterFactory(GsonConverterFactory.create())
-           .build()
-
-       val service = retrofit.create(ApiCall::class.java!!)
-       val call: Call<Card> = service.info
-
-       call.enqueue(object : Callback<Card> {
-           override fun onFailure(call: Call<Card>, t: Throwable) {
-               id.text =  "Failure: " + t.message
-           }
-
-           override fun onResponse(call: Call<Card>, response: Response<Card>) {
-               if(!response.isSuccessful){
-                   id.text = response.code().toString()
-                    return
-               }
-               var cards = response.body()
-               id.text =  "success: \n" + cards.toString()
-
-           }
-       })
-
+                }
+            }
+        })
     }
 }
-
-class Card{
-
-    var image: String = ""
-    var onClick: String = ""
-    //@SerializedName("cards")
-    //lateinit var text: String
+class Model {
+    lateinit var image:String
+    lateinit var onclick:String
+    constructor(image: String, onclick:String) {
+        this.image = image
+        this.onclick = onclick
+    }
+    constructor()
 }
-interface ApiCall {
-
-    @get:GET("cards")
-    var info: Call<Card>
-}
-
-
-
