6de54a98b84099a6cf6e6e3a55c4f8752734b8b3
Giovanny Cerna <gcerna@csumb.edu>
2019-12-10 16:22:29 -0800

    Finish api call and cards display

--- app/src/main/java/com/example/pokemonchallenge/MainActivity.kt
+++ app/src/main/java/com/example/pokemonchallenge/MainActivity.kt
@@ -1,14 +1,12 @@
 package com.example.pokemonchallenge
 
 import android.annotation.SuppressLint
-import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView.OnItemClickListener
 import android.widget.ImageView
-import android.widget.LinearLayout
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -17,11 +15,13 @@ import okhttp3.*
 import org.json.JSONArray
 import org.json.JSONObject
 import java.io.IOException
+
+
 class MainActivity : AppCompatActivity() {
 
-    var arrayListDetails:ArrayList<Model> = ArrayList()
+    var arrayListDetails:ArrayList<CardImages> = ArrayList()
     //OkHttpClient creates connection pool between client and server
-    val client = OkHttpClient()
+    private val client = OkHttpClient()
     lateinit var recyclerView: RecyclerView
 
 
@@ -31,7 +31,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         run("https://api.pokemontcg.io/v1/cards")
     }
-    fun run(url: String) {
+    private fun run(url: String) {
         val request = Request.Builder()
             .url(url)
             .build()
@@ -39,18 +39,40 @@ class MainActivity : AppCompatActivity() {
             override fun onFailure(call: Call, e: IOException) {
             }
             override fun onResponse(call: Call, response: Response) {
-                var strResponse = response.body()!!.string()
+                val strResponse = response.body()!!.string()
                 //creating json object
-                val jsonContact:JSONObject = JSONObject(strResponse)
+                val jsonContact = JSONObject(strResponse)
                 //creating json array
                 val jsonarrayInfo:JSONArray= jsonContact.getJSONArray("cards")
                 val size:Int = jsonarrayInfo.length()
+                var counter = 1
+                var model= CardImages()
+
                 for (i in 0 until size) {
                     val jsonObjectDetail:JSONObject = jsonarrayInfo.getJSONObject(i)
-                    val model:Model= Model()
-                    model.image = jsonObjectDetail.getString("imageUrl").toString()
-                    model.onclick = jsonObjectDetail.getString("imageUrlHiRes").toString()
-                    arrayListDetails.add(model)
+
+                    when (counter) {
+                        1 -> {
+                            model.image = jsonObjectDetail.getString("imageUrl").toString()
+                            model.onclick = jsonObjectDetail.getString("imageUrlHiRes").toString()
+                        }
+                        2 -> {
+                            model.image2 = jsonObjectDetail.getString("imageUrl").toString()
+                            model.onclick2 = jsonObjectDetail.getString("imageUrlHiRes").toString()
+                        }
+                        3 -> {
+                            model.image3 = jsonObjectDetail.getString("imageUrl").toString()
+                            model.onclick3 = jsonObjectDetail.getString("imageUrlHiRes").toString()
+                        }
+                        else -> {
+                            counter = 0
+                            model.image4 = jsonObjectDetail.getString("imageUrl").toString()
+                            model.onclick4 = jsonObjectDetail.getString("imageUrlHiRes").toString()
+                            arrayListDetails.add(model)
+                            model = CardImages()
+                        }
+                    }
+                    counter++
                 }
                 runOnUiThread {
                     recyclerView = findViewById(R.id.views)
@@ -62,24 +84,31 @@ class MainActivity : AppCompatActivity() {
         })
     }
 }
-class Model {
-    lateinit var image:String
-    lateinit var onclick:String
-    constructor(image: String, onclick:String) {
-        this.image = image
-        this.onclick = onclick
-    }
-    constructor()
+class CardImages {
+     var image:String = ""
+     var image2:String = ""
+     var image3:String = ""
+     var image4:String = ""
+
+     var onclick:String = ""
+     var onclick2:String = ""
+     var onclick3:String = ""
+     var onclick4:String = ""
 }
 
-class CustomAdapter(var list:ArrayList<Model>): RecyclerView.Adapter<CustomAdapter.ViewHolder>(){
+class CustomAdapter(private var list:ArrayList<CardImages>): RecyclerView.Adapter<CustomAdapter.ViewHolder>(){
 
     class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
-        val name = itemView.findViewById<ImageView>(R.id.imageView)
+        val card = itemView.findViewById<ImageView>(R.id.imageView)!!
+        val card2 = itemView.findViewById<ImageView>(R.id.imageView2)!!
+        val card3 = itemView.findViewById<ImageView>(R.id.imageView3)!!
+        val card4 = itemView.findViewById<ImageView>(R.id.imageView4)!!
+
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val v = LayoutInflater.from(parent.context).inflate(R.layout.card_list, parent, false)
+
         return ViewHolder(v)
     }
 
@@ -88,11 +117,23 @@ class CustomAdapter(var list:ArrayList<Model>): RecyclerView.Adapter<CustomAdapt
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val card :Model = list[position]
+        val card :CardImages = list[position]
         val picasso = Picasso.get()
-        picasso.load(card.image)
-          .into(holder.name)
-
-
+        if(card.image != "") {
+            picasso.load(card.image)
+                .into(holder.card)
+        }
+        if(card.image2 != "") {
+            picasso.load(card.image2)
+                .into(holder.card2)
+        }
+        if(card.image3 != "") {
+            picasso.load(card.image3)
+                .into(holder.card3)
+        }
+        if(card.image4 != "") {
+            picasso.load(card.image4)
+                .into(holder.card4)
+        }
     }
 }
